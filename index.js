1f346f6d2d061fc117c80523d42f131db80e9537
Takafumi Nakahara <kodawaripoteto@gmail.com>
2017-06-07 03:30:24 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+$(function() {
+    $('#navToggle').click(function(){//headerに .openNav を付加・削除
+        $('header').toggleClass('openNav');
+    });
+});
+
+$(function () {
+
+    //フィルター機能の実装
+    var $filters = $('.filter [data-filter]'),
+        $boxes = $('.contents-1st [data-category]');
+
+    
+    $filters.on('click', function(e) {
+      e.preventDefault();
+      var $this = $(this);
+      $filters.removeClass('active');
+      $this.addClass('active');
+
+      var $filterColor = $this.attr('data-filter');
+
+      console.log("click is ok. filter is " + $filterColor);
+
+      if ($filterColor == 'genres-all') {
+        $boxes.removeClass('is-animated')
+          .fadeOut().promise().done(function() {
+            $boxes.addClass('is-animated').fadeIn();
+          });
+      } else {
+        $boxes.removeClass('is-animated')
+          .fadeOut().promise().done(function() {
+            $boxes.filter('[data-category = "' + $filterColor + '"]')
+              .addClass('is-animated').fadeIn();
+          });
+      }
+    });
+
+    //ボタンを押したらスクロールの実装
+    //ページ内スクロール
+    $(".scroll-down-btn").click(function () {
+        var i = $(".scroll-down-btn").index(this)
+        var p = $("#profile").eq(i).offset().top;
+        $('html,body').animate({ scrollTop: p }, 700);
+        return false;
+    });
+
+    $(".scroll-up-btn").click(function () {
+        var i = $(".scroll-up-btn").index(this)
+        var p = $("#wrapper").eq(i).offset().top;
+        $('html,body').animate({ scrollTop: p }, 500);
+        return false;
+    });
+});
