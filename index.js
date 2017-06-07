db38951ebec7c714400fb8c8b57033eb907d3da0
Takafumi Nakahara <kodawaripoteto@gmail.com>
2017-06-07 11:33:14 +0900

    スマホ対応(index途中)

--- index.js
+++ index.js
@@ -27,40 +27,43 @@ $(function() {
 $(function() {
 
     var $firstview = $(".firstview");
+    var windowWidth = $(window).width();
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 160) {
-          $firstview.removeClass('firstview-scrolled-5 firstview-scrolled-4 firstview-scrolled-3 firstview-scrolled-2');
-          $firstview.addClass('firstview-scrolled-fin');
-          $(".space-of-firstview").addClass("display-space");
-          $(".scroll-down-btn").addClass("display-none");
-        } else if ($(this).scrollTop() > 100) {
-          $firstview.removeClass('firstview-scrolled-fin firstview-scrolled-4 firstview-scrolled-3 firstview-scrolled-2');
-          $firstview.addClass('firstview-scrolled-5');
-          $(".space-of-firstview").removeClass("display-space");
-          $(".scroll-down-btn").addClass("display-none");
-        } else if ($(this).scrollTop() > 40) {
-          $firstview.removeClass('firstview-scrolled-fin firstview-scrolled-5 firstview-scrolled-3 firstview-scrolled-2');
-          $firstview.addClass('firstview-scrolled-4');
-          $(".space-of-firstview").removeClass("display-space");
-          $(".scroll-down-btn").addClass("display-none");
-        } else if ($(this).scrollTop() > 30) {
-          $firstview.removeClass('firstview-scrolled-fin firstview-scrolled-5 firstview-scrolled-4 firstview-scrolled-2');
-          $firstview.addClass('firstview-scrolled-3');
-          $(".space-of-firstview").removeClass("display-space");
-          $(".scroll-down-btn").addClass("display-none");
-        } else if ($(this).scrollTop() > 20) {
-          $firstview.removeClass('firstview-scrolled-fin firstview-scrolled-5 firstview-scrolled-4 firstview-scrolled-3');
-          $firstview.addClass('firstview-scrolled-2');
-          $(".space-of-firstview").removeClass("display-space");
-          $(".scroll-down-btn").removeClass("display-none");
-        } else {
-          $firstview.removeClass('firstview-scrolled-fin firstview-scrolled-5 firstview-scrolled-4 firstview-scrolled-3 firstview-scrolled-2');
-          $(".space-of-firstview").removeClass("display-space");
-          $firstview.addClass('firstview');
-          $(".scroll-down-btn").removeClass("display-none");
-        }
-    });
+    if (windowWidth > 700){
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 160) {
+              $firstview.removeClass('firstview-scrolled-5 firstview-scrolled-4 firstview-scrolled-3 firstview-scrolled-2');
+              $firstview.addClass('firstview-scrolled-fin');
+              $(".space-of-firstview").addClass("display-space");
+              $(".scroll-down-btn").addClass("display-none");
+            } else if ($(this).scrollTop() > 100) {
+              $firstview.removeClass('firstview-scrolled-fin firstview-scrolled-4 firstview-scrolled-3 firstview-scrolled-2');
+              $firstview.addClass('firstview-scrolled-5');
+              $(".space-of-firstview").removeClass("display-space");
+              $(".scroll-down-btn").addClass("display-none");
+            } else if ($(this).scrollTop() > 40) {
+              $firstview.removeClass('firstview-scrolled-fin firstview-scrolled-5 firstview-scrolled-3 firstview-scrolled-2');
+              $firstview.addClass('firstview-scrolled-4');
+              $(".space-of-firstview").removeClass("display-space");
+              $(".scroll-down-btn").addClass("display-none");
+            } else if ($(this).scrollTop() > 30) {
+              $firstview.removeClass('firstview-scrolled-fin firstview-scrolled-5 firstview-scrolled-4 firstview-scrolled-2');
+              $firstview.addClass('firstview-scrolled-3');
+              $(".space-of-firstview").removeClass("display-space");
+              $(".scroll-down-btn").addClass("display-none");
+            } else if ($(this).scrollTop() > 20) {
+              $firstview.removeClass('firstview-scrolled-fin firstview-scrolled-5 firstview-scrolled-4 firstview-scrolled-3');
+              $firstview.addClass('firstview-scrolled-2');
+              $(".space-of-firstview").removeClass("display-space");
+              $(".scroll-down-btn").removeClass("display-none");
+            } else {
+              $firstview.removeClass('firstview-scrolled-fin firstview-scrolled-5 firstview-scrolled-4 firstview-scrolled-3 firstview-scrolled-2');
+              $(".space-of-firstview").removeClass("display-space");
+              $firstview.addClass('firstview');
+              $(".scroll-down-btn").removeClass("display-none");
+            }
+        });
+    }
 });
 
 
@@ -95,14 +98,27 @@ $(function () {
       }
     });
 
+
+    var windowWidth = $(window).width();
+
     //ボタンを押したらスクロールの実装
     //ページ内スクロール
-    $(".scroll-down-btn").click(function () {
-        var i = $(".scroll-down-btn").index(this)
-        var p = $(".profile").eq(i).offset().top;
-        $('html,body').animate({ scrollTop: 450 }, 700);
-        return false;
-    });
+    if (windowWidth > 700){
+        $(".scroll-down-btn").click(function () {
+            var i = $(".scroll-down-btn").index(this)
+            var p = $(".profile").eq(i).offset().top;
+            $('html,body').animate({ scrollTop: 450 }, 700);
+            return false;
+        });
+    }else {
+        $(".scroll-down-btn").click(function () {
+            var i = $(".scroll-down-btn").index(this)
+            var p = $(".profile").eq(i).offset().top - 80;
+            $('html,body').animate({ scrollTop: p }, 700);
+            return false;
+        });
+    }
+
 
     $(".scroll-up-btn").click(function () {
         var i = $(".scroll-up-btn").index(this)
