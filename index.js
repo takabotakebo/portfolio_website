cef4b05d97647ab5ac53b654f7a0d9236144f469
Takafumi Nakahara <kodawaripoteto@gmail.com>
2017-06-30 00:40:28 +0900

    スマホでのhoverに対応

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+var linkTouchStart = function(){
+    thisAnchor = $(this);
+    touchPos = thisAnchor.offset().top;
+    moveCheck = function(){
+        nowPos = thisAnchor.offset().top;
+        if(touchPos == nowPos){
+            thisAnchor.addClass("hover");
+        }
+    }
+    setTimeout(moveCheck,100);
+}
+var linkTouchEnd = function(){
+    thisAnchor = $(this);
+    hoverRemove = function(){
+        thisAnchor.removeClass("hover");
+    }
+    setTimeout(hoverRemove,500);
+}
+
+$(document).on('touchstart mousedown','a',linkTouchStart);
+$(document).on('touchend mouseup','a',linkTouchEnd);
+
+
 $(function() {
   $(document).ready(function() {
     $(".animsition-overlay").animsition({
